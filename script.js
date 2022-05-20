c6af3cbcab4ef92890fd3451a9f730645f374bc4
1Peripheral <1Peripheral@users.noreply.github.com>
2022-05-20 01:09:33 +0100

    Main functionality

---
+++ script.js
@@ -0,0 +1,25 @@
+var textBox = document.getElementById('input-text');
+var cipherKey = document.getElementById('key-text');
+
+
+function Encrypt(){
+    let encrypted = CryptoJS.AES.encrypt(textBox.value, cipherKey.value);
+    
+    textBox.value = encrypted;
+}
+
+
+function Decrypt() {
+    let decrypted = CryptoJS.AES.decrypt(textBox.value, cipherKey.value).toString(CryptoJS.enc.Utf8);
+
+    textBox.value = decrypted;
+}
+
+
+function Clear() {
+    let check = document.getElementById('lock-check').checked;
+    textBox.value = "";
+
+    if (!check)
+        cipherKey.value = ""; 
+}
